073b636810ec6cc54e66739fe284af234b03731c
halilcanturan <c.turan0405@gmail.com>
2024-02-16 08:59:26 +0300

    Add project files.

---
+++ Controllers/RolesController.cs
@@ -0,0 +1,92 @@
+﻿using IdentityApp.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityApp.Controllers
+{
+    [Authorize]
+    public class RolesController : Controller
+    {
+        private readonly RoleManager<AppRole> _roleManager;
+        private readonly UserManager<AppUser> _userManager;
+        public RolesController(RoleManager<AppRole> roleManager, UserManager<AppUser> userManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_roleManager.Roles);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(AppRole model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _roleManager.CreateAsync(model);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                foreach (var err in result.Errors)
+                {
+                    ModelState.AddModelError("", err.Description);
+                }
+
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> Edit(string Id)
+        {
+            var role = await _roleManager.FindByIdAsync(Id);
+
+            if (role != null && role.Name != null)
+            {
+                ViewBag.Users = await _userManager.GetUsersInRoleAsync(role.Name); //rolü isimlerine göre getiriyor
+                return View(role);
+            }
+
+            return RedirectToAction("Indedx");
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(AppRole model)
+        {
+            if(ModelState.IsValid)
+            {
+                var role = await _roleManager.FindByIdAsync(model.Id);
+
+                if(role!=null)
+                {
+                    role.Name = model.Name;
+
+                    var result = await _roleManager.UpdateAsync(role);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                    foreach (var err in result.Errors)
+                    {
+                        ModelState.AddModelError("", err.Description);
+                    }
+
+                    if (role.Name != null)
+                        ViewBag.Users = await _userManager.GetUsersInRoleAsync(role.Name);
+                }
+            }
+            return View(model);
+        }
+    }
+}
---
+++ Models/IdentitySeedData.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityApp.Models
+{
+    public class IdentitySeedData
+    {
+        private const string adminUser = "Admin";
+        private const string adminPassword = "Admin_123";
+
+        public static async void IdentityTestUser(IApplicationBuilder app)
+        {
+            var context = app.ApplicationServices.CreateScope().ServiceProvider.GetRequiredService<IdentityContext>();
+
+            if (context.Database.GetAppliedMigrations().Any())
+            {
+                context.Database.Migrate();
+            }
+
+            var userManager = app.ApplicationServices.CreateScope().ServiceProvider.GetRequiredService<UserManager<AppUser>>();
+
+            var user = await userManager.FindByNameAsync(adminUser);
+            if (user == null)
+            {
+                user = new AppUser
+                {
+                    FullName="Can Turan",
+                    UserName = adminUser,
+                    Email = "admin@canturan.com",
+                    PhoneNumber = "123456",
+                };
+
+                await userManager.CreateAsync(user, adminPassword);
+
+            }
+
+        }
+    }
+}
